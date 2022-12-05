2a17f2b76fc35bc187ca4135e23811325735a1b1
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2022-12-05 18:58:28 -0300

    Configura tela de boas vindas.

--- src/main/java/dcc025/bingo/Bingo.java
+++ src/main/java/dcc025/bingo/Bingo.java
@@ -9,9 +9,26 @@ package dcc025.bingo;
  *
  * @author thiago
  */
+import java.util.*;
 public class Bingo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner teclado = new Scanner(System.in);
+        int numJogadores;
+        String modoJogo;
+        
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("                            B  I  N  G  O");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("Bem Vindo ao Bingo!");
+        
+        System.out.println("\n_____________________________"); 
+        System.out.println("\nDigite o número de jogadores: ");
+        numJogadores = teclado.nextInt();
+        
+        System.out.println("Digite o modo de jogo:");
+        System.out.println("1 - Linha\n2 - Cruz");
+        modoJogo = teclado.next();
+        System.out.println("_____________________________"); 
     }
 }
