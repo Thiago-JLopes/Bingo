6315e3d8de0cb0aa346abe8565d975e8cad92d52
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2022-12-11 18:56:21 -0300

    Altera o imprimir, adiciona os números ordenados na cartela, cria marcador da cartela.

--- src/main/java/dcc025/bingo/Cartela.java
+++ src/main/java/dcc025/bingo/Cartela.java
@@ -16,7 +16,7 @@ public class Cartela {
     private static int numJogadores;
     private static String modoJogo;
     private static List<String[][]> listaCartelas;
-    DecimalFormat nf = new DecimalFormat("00");
+    DecimalFormat nf = new DecimalFormat("00");//formatar números da cartela
 
     public Cartela(int numJogadores, String modoJogo) {
         listaCartelas = new ArrayList<>();
@@ -24,44 +24,62 @@ public class Cartela {
         Cartela.modoJogo = modoJogo.trim();
     }
 
-    public void preecherCartela() {
-        for (int n = 0; n < numJogadores; n++) {
-            GeradorNumero numAleatorio = new GeradorNumero();
-            String cartela[][] = new String[6][5];
+    public void criarCartelas() {
 
+        for (int n = 0; n < numJogadores; n++) {//cria n cartelas e adicona na lista de cartelas
+
+            String cartela[][] = new String[6][5];
+            PreecherCartelas newCartela = new PreecherCartelas();
+
+            //Cria vetores para armazenar os números sem repetição.
+            int colunaB[] = new int[5];
+            int colunaI[] = new int[5];
+            int colunaN[] = new int[5];
+            int colunaG[] = new int[5];
+            int colunaO[] = new int[5];
+
+            //Chama método para preencher os vetores e ordenar.
+            newCartela.preencherColuna(colunaB, 1, 16);
+            newCartela.preencherColuna(colunaI, 16, 31);
+            newCartela.preencherColuna(colunaN, 31, 46);
+            newCartela.preencherColuna(colunaG, 46, 61);
+            newCartela.preencherColuna(colunaO, 61, 76);
+
+            //Define a primeira linha da matriz cartela.
             cartela[0][0] = " B   ";
             cartela[0][1] = " I   ";
             cartela[0][2] = " N   ";
             cartela[0][3] = " G   ";
             cartela[0][4] = " O   ";
 
-            for (int i = 1; i < 6; i++) {
-                for (int j = 0; j < 5; j++) {
+            //Preecher a cartela com o vetor já ordenado.
+            for (int j = 0; j < 5; j++) {
+                for (int i = 1; i < 6; i++) {
 
                     if (i == 3 && j == 2) {
                         cartela[i][j] = "     ";
                     } else {
-
                         switch (j) {
                             case 0:
-                                cartela[i][j] = "[ ]" + nf.format(numAleatorio.aleatorio(1, 16));
+                                cartela[i][j] = "[ ]" + nf.format(colunaB[i - 1]);
                                 break;
                             case 1:
-                                cartela[i][j] = "[ ]" + nf.format(numAleatorio.aleatorio(16, 31));
+                                cartela[i][j] = "[ ]" + colunaI[i - 1];
                                 break;
                             case 2:
-                                cartela[i][j] = "[ ]" + nf.format(numAleatorio.aleatorio(31, 46));
+                                cartela[i][j] = "[ ]" + colunaN[i - 1];
                                 break;
                             case 3:
-                                cartela[i][j] = "[ ]" + nf.format(numAleatorio.aleatorio(46, 61));
+                                cartela[i][j] = "[ ]" + colunaG[i - 1];
                                 break;
                             case 4:
-                                cartela[i][j] = "[ ]" + nf.format(numAleatorio.aleatorio(61, 76));
+                                cartela[i][j] = "[ ]" + colunaO[i - 1];
                                 break;
                             default:
                                 break;
                         }
                     }
+
                 }
             }
 
@@ -69,10 +87,19 @@ public class Cartela {
         }
     }
 
-    public void imprime() {
+    public void imprime(List marcadores, int sorteado) {
         int n = 1;
+        
+        System.out.println("O número sorteado foi: " + sorteado + "\n");
+        if(marcadores.isEmpty()) {
+            System.out.println("Ninguém marcou nesta rodada");
+        } else {
+            for (int i = 0; i < marcadores.size(); i++) {
+                System.out.println("O jogador " + marcadores.get(i)+ " marcou.");
+            }
+        }
         for (String[][] leCartela : listaCartelas) {
-            System.out.println("Cartela " + n);
+            System.out.println("\nCartela " + n);
             for (int i = 0; i < 6; i++) {
                 for (int j = 0; j < 5; j++) {
                     System.out.print(leCartela[i][j] + "   ");
@@ -84,18 +111,35 @@ public class Cartela {
         }
     }
 
-    public void ganhador(int sorteado) {
+    public void marcaCartelas(int sorteado) {
+        int numSorteado = sorteado;
+        List<Integer> marcadores = new ArrayList<>(); // Lista para guardar os jogadores que marcaram na rodada.
         int n = 0;
         while (n < numJogadores) {
             for (int i = 0; i < 6; i++) {
                 for (int j = 0; j < 5; j++) {
-                    if (listaCartelas.get(n)[i][j].equals("[ ]" + nf.format(sorteado))) {
-                        listaCartelas.get(n)[i][j] = "[x]" + nf.format(sorteado);
+                    if (listaCartelas.get(n)[i][j].equals("[ ]" + nf.format(numSorteado))) {
+                        listaCartelas.get(n)[i][j] = "[x]" + nf.format(numSorteado);
+                        marcadores.add(n+1);
                     }
                 }
             }
             n++;
         }
+        
+        imprime(marcadores, numSorteado);
     }
 
+    /*public void ganhador(int sorteado) {
+        int n = 0;
+        while (n < numJogadores) {
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (listaCartelas.get(n)[i][j].equals("[ ]" + nf.format(sorteado))) {
+                    }
+                }
+            }
+            n++;
+        }
+    }*/
 }
