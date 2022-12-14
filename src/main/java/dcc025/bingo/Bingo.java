8c67916c04d9ff0c588f95aaf65d35ee5adfe4d4
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2022-12-14 00:22:58 -0300

    Adiciona loop para o sorteio e opção de continuar ou parar o programa quanto houver ganhador

--- src/main/java/dcc025/bingo/Bingo.java
+++ src/main/java/dcc025/bingo/Bingo.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package dcc025.bingo;
 
 /**
@@ -10,33 +9,47 @@ package dcc025.bingo;
  * @author thiago
  */
 import java.util.*;
+
 public class Bingo {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int numJogadores;
         String modoJogo;
-        
+        String continuar = "S";
+
         System.out.println("------------------------------------------------------------------------");
         System.out.println("                       B     I     N     G     O");
         System.out.println("------------------------------------------------------------------------");
         System.out.println("Bem Vindo ao Bingo!");
-        
-        System.out.println("\n_____________________________"); 
+
+        System.out.println("\n_____________________________");
         System.out.println("\nDigite o número de jogadores: ");
         numJogadores = teclado.nextInt();
-        
+
         System.out.println("Digite o modo de jogo:");
-        System.out.println("1 - Linha\n2 - Cruz");
+        System.out.println("1 - Linha/Coluna\n2 - Cruz");
         modoJogo = teclado.next();
         System.out.println("_____________________________");
-        
+
         Cartela newCartela = new Cartela(numJogadores, modoJogo);
         newCartela.criarCartelas();
-        newCartela.marcaCartelas(5);
-        //newCartela.imprime();
-        //newCartela.ganhador(4);
-       // newCartela.imprime();
-        
+
+        List<Integer> jaSorteados = new ArrayList<>();
+        GeradorNumero sorteado = new GeradorNumero();
+
+        do {
+            int valSorteado = sorteado.aleatorio(1, 75);
+            jaSorteados.add(valSorteado);
+            newCartela.marcaCartelas(valSorteado);
+
+            if (!Cartela.ganhador()) {
+                System.out.println("Deseja continuar? (S/N)");
+                continuar = teclado.next();
+            } else {
+                break;
+            }
+        } while (continuar.equalsIgnoreCase("S"));
+
     }
 }
